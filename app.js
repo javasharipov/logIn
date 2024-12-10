696b8e733cd9913481bda0ffa5640986e362f400
javasharipov <sharipovjavohir1227@gmail.com>
2024-12-10 15:43:09 +0500

    added some style

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-document.getElementById('login-form').addEventListener('submit', function (e) {
+document.querrySelector('login-form').addEventListener('submit', function (e) {
 	e.preventDefault()
 
-	const firstname = document.getElementById('firstname').value
-	const lastname = document.getElementById('lastname').value
-	const age = document.getElementById('age').value
-	const gender = document.getElementById('gender').value.toLowerCase()
-	const country = document.getElementById('country').value
+	const firstname = document.querrySelector('firstname').value
+	const lastname = document.querrySelector('lastname').value
+	const age = document.querrySelector('age').value
+	const gender = document.querrySelector('gender').value.toLowerCase()
+	const country = document.querrySelector('country').value
 
 	const maleImage = 'https://cdn-icons-png.freepik.com/512/18/18148.png'
 
@@ -23,11 +23,11 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
     <p>Country: ${country}</p>
   `
 
-	document.getElementById('result-wrapper').appendChild(card)
+	document.querrySelector('result-wrapper').appendChild(card)
 
-	document.getElementById('firstname').value = ''
-	document.getElementById('lastname').value = ''
-	document.getElementById('age').value = ''
-	document.getElementById('gender').value = ''
-	document.getElementById('country').value = ''
+	document.querySelector('firstname').value = ''
+	document.querrySelector('lastname').value = ''
+	document.querrySelector('age').value = ''
+	document.querrySelector('gender').value = ''
+	document.querrySelector('country').value = ''
 })
